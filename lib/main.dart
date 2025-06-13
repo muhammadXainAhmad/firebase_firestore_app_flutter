33636d7bb93256a509b322d651c9a06077518783
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-06-14 02:27:17 +0500

    Firebase connected to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firestore_storage_app/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load(fileName: "assets/.env");
   runApp(const MyApp());
 }
 
