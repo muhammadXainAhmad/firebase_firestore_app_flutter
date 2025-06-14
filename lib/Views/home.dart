d9fab847c6ef8d9dc01824f8c24c2a0bb9caa26b
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-06-15 02:11:48 +0500

    Created a bottomSheet to addData

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firestore_storage_app/Utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -9,6 +10,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+
   Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
   }
@@ -17,33 +22,182 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.black,
-        child: Icon(Icons.add, color: Colors.white),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.home, color: Colors.blue, size: 250),
-            Padding(
-              padding: const EdgeInsets.only(left: 12.0, right: 12, bottom: 8),
-              child: ElevatedButton(
-                onPressed: () async {
-                  await signOut();
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return StatefulBuilder(
+                builder: (context, setModalState) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        top: BorderSide(color: Colors.black),
+                        right: BorderSide(color: Colors.black),
+                        left: BorderSide(color: Colors.black),
+                      ),
+                      color: Colors.orangeAccent.shade100,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(12),
+                          child: Text(
+                            "Add New Task",
+                            style: TextStyle(fontSize: 24),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12.0,
+                            right: 12,
+                            bottom: 8,
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              DateTime? pickedDate = await showDatePicker(
+                                context: context,
+                                firstDate: DateTime(2000),
+                                lastDate: DateTime(2050),
+                                initialDate: DateTime.now(),
+                              );
+                              if (pickedDate != null) {
+                                setModalState(() {
+                                  selectedDate = pickedDate;
+                                });
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(double.infinity, 50),
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            child: Text(
+                              selectedDate.toString().split(" ").first,
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12.0,
+                            right: 12,
+                            bottom: 8,
+                          ),
+                          child: TextField(
+                            maxLines: 2,
+                            decoration: InputDecoration(
+                              hintText: "Title",
+                              hintStyle: TextStyle(color: Colors.black),
+                              fillColor: Colors.white,
+                              filled: true,
+                              enabledBorder: MyConstants.eBorder,
+                              focusedBorder: MyConstants.fBorder,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12.0,
+                            right: 12,
+                            bottom: 12,
+                          ),
+                          child: TextField(
+                            maxLines: 5,
+                            decoration: InputDecoration(
+                              hintText: "Description",
+                              hintStyle: TextStyle(color: Colors.black),
+                              fillColor: Colors.white,
+                              filled: true,
+                              enabledBorder: MyConstants.eBorder,
+                              focusedBorder: MyConstants.fBorder,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12.0,
+                            right: 12,
+                            bottom: 8,
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () async {},
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(double.infinity, 50),
+                              backgroundColor: Colors.black,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              "SIGN IN",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
                 },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 50),
-                  backgroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+              );
+            },
+          );
+        },
+        backgroundColor: Colors.orangeAccent.shade100,
+        child: Icon(Icons.add),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () async {
+                await signOut();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                child: Text("SIGN OUT", style: TextStyle(color: Colors.white)),
               ),
+              child: Text("SIGN OUT", style: TextStyle(color: Colors.white)),
             ),
           ],
         ),
+      ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("MY TASKS", style: TextStyle(color: Colors.black)),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        title: Text("Hello"),
+                        tileColor: Colors.orangeAccent.shade100,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
